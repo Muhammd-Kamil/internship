114ec7cc29c52bf4640987637e3dd8bd530fd0fe
Muhammad-Kamil <maaz.alam00@gmail.com>
2023-08-22 13:54:15 +0530

    new codes from kamil

---
+++ src/components/Departments.tsx
@@ -0,0 +1,83 @@
+import * as React from "react";
+import {useState} from "react";
+import { styled } from "@mui/material/styles";
+// import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import Checkbox from "@mui/material/Checkbox";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import MuiAccordionSummary, {
+  AccordionSummaryProps,
+} from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import { departmentData } from "../services/data";
+import { Typography } from "@mui/material";
+
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
+
+const AccordionSummary = styled((props: AccordionSummaryProps) => (
+  <MuiAccordionSummary
+    // expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
+  },
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
+
+export default function Departments() {
+  const [data, setData] = useState(departmentData);
+
+  const [expanded, setExpanded] = React.useState<string | false>("panel1");
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
+
+//   const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+  return (
+    <div>
+      <Checkbox />
+      {data.map((a) => {
+        return (
+          <Accordion
+            expanded={expanded === "panel1"}
+            onChange={handleChange("panel1")}
+          >
+            <AccordionSummary
+              aria-controls="panel1d-content"
+              id="panel1d-header"
+            >
+              <Typography> {a.department}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>{a.sub_departments}</AccordionDetails>
+          </Accordion>
+        );
+      })}
+    </div>
+  );
+}
